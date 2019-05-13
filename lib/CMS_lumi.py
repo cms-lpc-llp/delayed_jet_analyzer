fa30c2ba36acfdef7bedc7b943a2ac00cf558095
Christina Wenlu Wang <christiw@flere-imsaho-sm.hep.caltech.edu>
2019-05-12 17:41:01 -0700

    update

--- lib/CMS_lumi.py
+++ lib/CMS_lumi.py
@@ -25,7 +25,7 @@ relExtraDY = 1.2
 
 extraOverCmsTextSize  = 0.76
 
-lumi_13TeV = "20.1 fb^{-1}"
+lumi_13TeV = "137 fb^{-1}"
 lumi_8TeV  = "19.7 fb^{-1}"
 lumi_7TeV  = "5.1 fb^{-1}"
 lumi_sqrtS = ""
